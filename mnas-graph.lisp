d34dc775abcce3421fa81ac80fcc94b404a55b56
Nick Matvyeyev <mnasoft@gmail.com>
2018-08-10 06:47:56 +0300

    2018-8-10 6:47:52 mnasoft-00

--- mnas-graph.lisp
+++ mnas-graph.lisp
@@ -5,6 +5,8 @@
 (defpackage #:mnas-graph
   (:use #:cl #:mnas-hash-table))
 
+(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
+
 (in-package #:mnas-graph)
 
 ;;;; generics ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -18,26 +20,32 @@
 (export 'remove-from)
 (defgeneric remove-from (obj container) (:documentation "Добавляет obj в container"))
 
-(export 'inlet-vers)
-(defgeneric inlet-vers (container) (:documentation "Возвращает хеш-таблицу конечных вершин (вершин-стока)"))
+(export 'inlet-nodes)
+(defgeneric inlet-nodes (container) (:documentation "Возвращает хеш-таблицу конечных вершин (вершин-стока)"))
 
-(export 'outlet-vers)
-(defgeneric outlet-vers (container) (:documentation "Возвращает хеш-таблицу начальных вершин (веншин-иточников)"))
+(export 'outlet-nodes)
+(defgeneric outlet-nodes (container) (:documentation "Возвращает хеш-таблицу начальных вершин (веншин-иточников)"))
 
 (export 'inlet-ribs)
-(defgeneric inlet-ribs (container ver) (:documentation "Возвращает хеш-таблицу начальных ребер (итоков)"))
+(defgeneric inlet-ribs (container node) (:documentation "Возвращает хеш-таблицу начальных ребер (итоков)"))
 
 (export 'outlet-ribs)
-(defgeneric outlet-ribs (container ver) (:documentation "Возвращает хеш-таблицу конечных ребер (устий)"))
+(defgeneric outlet-ribs (container node) (:documentation "Возвращает хеш-таблицу конечных ребер (устий)"))
+
+(export 'graph-find-node-by-name)
+(defgeneric graph-find-node-by-name (container node-name) (:documentation "Поиск вершины по имени"))
+
+(export 'graph-find-edge-by-name)
+(defgeneric graph-find-edge-by-name (container edge-name) (:documentation "Поиск ребра по имени"))
 
 (export 'to-graphviz)
-(defgeneric to-graphviz (obj stream) (:documentation "Выполняет вывод в формат программы graphviz"))
+(defgeneric to-graphviz (obj stream) (:documentation "Выполняет объекта obj в формат программы graphviz"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(export 'ver)
+(export 'node)
 
-(defclass ver ()
+(defclass node ()
   ((node    :accessor vertex-node    :initarg :node   :initform nil  :documentation "Имя вершины")
    (counter :accessor vertex-counter                  :initform 0    :documentation "Количество, созданных вершин" :allocation :class))
    (:documentation "Вершина графа"))
@@ -49,7 +57,7 @@
 
 (export 'vertex)
 
-(defclass vertex (ver)
+(defclass vertex (node)
   ((number  :accessor vertex-number  :initarg :number                :documentation "Номер вершины")
    (state   :accessor vertex-state   :initarg :state  :initform nil  :documentation "Ссылка на состояние вершины"))
    (:documentation "Вершина графа поддерживающая номер и состояние"))
@@ -59,32 +67,32 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(export 'rib)
+(export 'edge)
 
-(defclass rib ()
-  ((start :accessor rib-from :initarg :from :initform nil :documentation "Начальная вершина ребра")
-   (end   :accessor rib-to   :initarg :to   :initform nil :documentation "Конечная  вершина ребра"))
+(defclass edge ()
+  ((start :accessor edge-from :initarg :from :initform nil :documentation "Начальная вершина ребра")
+   (end   :accessor edge-to   :initarg :to   :initform nil :documentation "Конечная  вершина ребра"))
   (:documentation "Ребро графа"))
 
 
-(export 'rib-from)
-(export 'rib-to)
+(export 'edge-from)
+(export 'edge-to)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (export 'graph)
 
 (defclass graph ()
-  ((vertexes :accessor graph-vers :initform (make-hash-table) :documentation "Хешированная таблица вершин графа")
-   (ribs     :accessor graph-ribs :initform (make-hash-table) :documentation "Хешированная таблица ребер графа"))
+  ((vertexes  :accessor graph-nodes :initform (make-hash-table) :documentation "Хешированная таблица вершин графа")
+   (edges     :accessor graph-edges :initform (make-hash-table) :documentation "Хешированная таблица ребер графа"))
   (:documentation "Представляет граф, выражающий алгоритм изменения состояния агрегатов во времени"))
 
-(export 'graph-vers)
-(export 'graph-ribs)
+(export 'graph-nodes)
+(export 'graph-edges)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmethod initialize-instance :around ((x ver) &key node )
+(defmethod initialize-instance :around ((x node) &key node )
   (call-next-method x
 		    :node   node 
 		    :number (vertex-counter x))
@@ -100,7 +108,7 @@
 
 ;;;;;;;;;; print-object ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmethod print-object :after ((x ver) s)
+(defmethod print-object :after ((x node) s)
 	   (format s "~S(~S)~%" (vertex-counter x) (vertex-node x)))
 
 (defmethod print-object :after ((x vertex) s)
@@ -108,20 +116,20 @@
 		   (vertex-number x)
 		   (vertex-state  x)))
 
-(defmethod print-object :after ((x rib) s)
-  (format s "(~S->~S)" (rib-from x) (rib-to x)))
+(defmethod print-object :after ((x edge) s)
+  (format s "(~S->~S)" (edge-from x) (edge-to x)))
 
 (defmethod print-object :after ((x graph) s)
   (format s "(VC=~S RC=~S"
-	  (hash-table-count (graph-vers x))
-	  (hash-table-count (graph-ribs x)))
-  (when (< 0 (hash-table-count (graph-vers x)))
+	  (hash-table-count (graph-nodes x))
+	  (hash-table-count (graph-edges x)))
+  (when (< 0 (hash-table-count (graph-nodes x)))
     (format s ")~%(" )
-    (maphash #'(lambda (k v) (format s "~S " v) )(graph-vers x))
+    (maphash #'(lambda (k v) (format s "~S " v) )(graph-nodes x))
     (format s ")" ))
-  (when (< 0 (hash-table-count (graph-ribs x)))
+  (when (< 0 (hash-table-count (graph-edges x)))
     	(format s "~%(" )
-	(maphash #'(lambda (k v) (format s "~S~%" v) )(graph-ribs x))
+	(maphash #'(lambda (k v) (format s "~S~%" v) )(graph-edges x))
 	(format s ")"))
   (format s ")"))
 
@@ -129,119 +137,115 @@
 
 (defmethod to-string (val) (format nil "~A" val))
 
-(defmethod to-string ((x ver)) (format nil "~A" (vertex-node x)))
+(defmethod to-string ((x node)) (format nil "~A" (vertex-node x)))
 
 (defmethod to-string ((x vertex)) (format nil "~A:~A" (vertex-node x) (vertex-number x)))
 
-(defmethod to-string ((x rib))
-  (format nil "~A->~A" (to-string (rib-from x)) (to-string (rib-to x))))
+(defmethod to-string ((x edge))
+  (format nil "~A->~A" (to-string (edge-from x)) (to-string (edge-to x))))
 
 ;;;;;;;;;; insert-to ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmethod insert-to ((v ver) (g graph)) (setf (gethash v (graph-vers g)) v) v)
+(defmethod insert-to ((v node) (g graph)) (setf (gethash v (graph-nodes g)) v) v)
 
-(defmethod insert-to ((r rib) (g graph))
-  (setf (gethash r (graph-ribs g)) r)
-  (setf (gethash (rib-from r) (graph-vers g)) (rib-from r))
-  (setf (gethash (rib-to   r) (graph-vers g)) (rib-to   r))
+(defmethod insert-to ((r edge) (g graph))
+  (setf (gethash r (graph-edges g)) r)
+  (setf (gethash (edge-from r) (graph-nodes g)) (edge-from r))
+  (setf (gethash (edge-to   r) (graph-nodes g)) (edge-to   r))
   r)
 
 ;;;;;;;;;; remove-from ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmethod remove-from ((v ver) (g graph ))
-  (let* ((rh (graph-ribs g))
+(defmethod remove-from ((v node) (g graph ))
+  (let* ((rh (graph-edges g))
 	 (rl (hash-table-copy rh)))
     (maphash #'(lambda(key val)
 		 (if (or
-		      (eq (rib-from key) v)
-		      (eq (rib-to key)   v))
+		      (eq (edge-from key) v)
+		      (eq (edge-to key)   v))
 		     (remhash key rh)))
 	     rl)
-    (if (remhash v (graph-vers g))
+    (if (remhash v (graph-nodes g))
 	v)))
 
-(defmethod remove-from ((r rib) (g graph ) )
-  (if (remhash r (graph-ribs g))
+(defmethod remove-from ((r edge) (g graph ) )
+  (if (remhash r (graph-edges g))
 	r))
 
 ;;;;;;;;;; graph-clear ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defmethod graph-clear ((g graph))
-  (clrhash (graph-vers g))
-  (clrhash (graph-ribs g))
+  (clrhash (graph-nodes g))
+  (clrhash (graph-edges g))
   g)
 
 
-;;;;;;;;;;  graph-inlet graph-outlet ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;  inlet outlet ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmethod inlet-vers ((g graph))
-  (let ((rez-tbl(hash-table-copy (graph-vers g))))
+(defmethod inlet-nodes ((g graph))
+  (let ((rez-tbl(hash-table-copy (graph-nodes g))))
     (maphash
      #'(lambda (k v)
-	 (remhash (rib-to k) rez-tbl))
-     (graph-ribs g))
+	 (remhash (edge-to k) rez-tbl))
+     (graph-edges g))
     rez-tbl))
 
-(defmethod outlet-vers ((g graph))
-  (let ((rez-tbl(hash-table-copy (graph-vers g))))
+(defmethod outlet-nodes ((g graph))
+  (let ((rez-tbl(hash-table-copy (graph-nodes g))))
     (maphash
      #'(lambda (k v)
-	 (remhash (rib-to k) rez-tbl))
-     (graph-ribs g))
+	 (remhash (edge-to k) rez-tbl))
+     (graph-edges g))
     rez-tbl))
 
-(defmethod outlet-ribs ((g graph) (v ver))
-  (let ((rez-tbl(hash-table-copy(graph-ribs g))))
+(defmethod outlet-edges ((g graph) (v node))
+  (let ((rez-tbl(hash-table-copy(graph-edges g))))
     (maphash
      #'(lambda (key val)
-	 (if (not(eq (rib-from key) v))
+	 (if (not(eq (edge-from key) v))
 	     (remhash  key rez-tbl)))
-     (graph-ribs g))
+     (graph-edges g))
     rez-tbl))
 
-(defmethod inlet-ribs ((g graph) (v ver))
-  (let ((rez-tbl (hash-table-copy (graph-ribs g))))
+(defmethod inlet-edges ((g graph) (v node))
+  (let ((rez-tbl (hash-table-copy (graph-edges g))))
     (maphash
      #'(lambda (key val)
-	 (if (not(eq (rib-to key) v))
+	 (if (not(eq (edge-to key) v))
 	     (remhash  key rez-tbl)))
-     (graph-ribs g))
+     (graph-edges g))
     rez-tbl))
 
 ;;;;;;;;;; graph-find-* ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(export 'graph-find-vertex-by-name)
-
-(defmethod graph-find-vertex-by-name ((g graph) (str string))
-  (let ((ver nil))
+(defmethod graph-find-node-by-name ((g graph) (str string))
+  (let ((v nil))
     (maphash #'(lambda (k v)
 	       (if (string= (to-string k) str)
-		   (setf ver k))
+		   (setf v k))
 	       )
-	     (graph-vers g))
-    ver))
+	     (graph-nodes g))
+    v))
 
-(export 'graph-find-rib-by-name)
-
-(defmethod graph-find-rib-by-name ((g graph) (str string))
+(defmethod graph-find-edge-by-name ((g graph) (str string))
   (let ((rb nil))
     (maphash #'(lambda (k v)
 	       (if (string= (to-string k) str)
 		   (setf rb k))
 	       )
-	     (graph-ribs g))
+	     (graph-edges g))
     rb))
 
 ;;;; to-graphviz ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmethod to-graphviz ((v ver) s)
+(defmethod to-graphviz ((v node) s)
   (format s "~S~%" (to-string v)))
 
-(defmethod to-graphviz ((r rib) s)
+(defmethod to-graphviz ((r edge) s)
   (format s "~S ~A ~S~%"
-	  (to-string (rib-from r))
+	  (to-string (edge-from r))
 	  "->"
-	  (to-string (rib-to r))))
+	  (to-string (edge-to r))))
 
 (defun x-preamble(&key (out t) (name "G") (rankdir "LR") (shape "box"))
   (format out "digraph ~A {~%  rankdir=~A~%  node[shape=~A]~%" name rankdir shape))
@@ -250,21 +254,64 @@
 
 (defmethod to-graphviz ((g graph) s)
   (x-preamble :out s)
-  (maphash #'(lambda (k v) (to-graphviz v s)) (graph-vers g))  
-  (maphash #'(lambda (k v) (to-graphviz v s)) (graph-ribs g))  
+  (maphash #'(lambda (k v) (to-graphviz v s)) (graph-nodes g))  
+  (maphash #'(lambda (k v) (to-graphviz v s)) (graph-edges g))  
   (x-postamble :out s))
 
 ;;;; generate-graph data ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(export 'generate-graph)
+
 (defun generate-graph (data)
   (let ((g (make-instance 'graph))
 	(vs (remove-duplicates (apply #'append data) :test #'equal)))
-    (mapc #'(lambda (v) (insert-to (make-instance 'ver :node v) g)) vs)
+    (mapc #'(lambda (v) (insert-to (make-instance 'node :node v) g)) vs)
     (mapc #'(lambda (el)
 	      (insert-to
-	       (make-instance 'rib
-			      :from (graph-find-vertex-by-name g (first el))
-			      :to   (graph-find-vertex-by-name g (second el)))
+	       (make-instance 'edge
+			      :from (graph-find-node-by-name g (first el))
+			      :to   (graph-find-node-by-name g (second el)))
 	       g))
 	  data)
     g))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(export '*dot-path*)
+
+(defparameter *dot-path*
+  (cond ((uiop/os:os-windows-p) "D:/PRG/msys32/mingw32/bin/dot.exe")
+	((uiop/os:os-unix-p) "/usr/bin/dot")))
+
+(export '*viewer-path*)
+
+(defparameter *output-path*
+  (cond ((uiop/os:os-windows-p) "D:/PRG/msys32/home/namatv/quicklisp/local-projects/clisp/algorithm") 
+	((uiop/os:os-unix-p) "/home/namatv/quicklisp/local-projects/clisp/algorithm/rezult")))
+
+(export '*viewer-path*)
+
+(defparameter *viewer-path*
+  (cond
+    ((uiop/os:os-windows-p) "C:/Program Files/Adobe/Reader 11.0/Reader/AcroRd32.exe")
+    ((uiop/os:os-unix-p) "/usr/bin/atril"))) ;;;;"/usr/bin/okular"
+
+(export 'view-graph)
+
+(defmethod view-graph ((g graph) fname
+		       &key
+			 (out-type "pdf") (dpi "150") (viewer *viewer-path*)
+			 (dot-prg *dot-path*) (fpath *output-path*) (invoke-viewer t))
+  (with-open-file (out (concatenate 'string fpath "/" fname ".gv")
+		       :direction :output :if-exists :supersede :external-format :UTF8)
+    (to-graphviz g out))
+  (sb-ext:run-program dot-prg
+		      (list (concatenate 'string "-T" out-type)
+			    (concatenate 'string "-Gdpi=" dpi)
+			    "-o"
+			    (concatenate 'string fpath "/" fname ".gv" "." out-type)
+			    (concatenate 'string fpath "/" fname ".gv")))
+  (when invoke-viewer
+      (sb-ext:run-program viewer
+			  (list (concatenate 'string fpath "/" fname ".gv" "." out-type)))))
+
--- testing.lisp
+++ testing.lisp
@@ -4,18 +4,18 @@
 
 (progn
   (defparameter *g*     (make-instance 'graph ))
-  (defparameter *v1-v2* (make-instance 'rib
-			    :from (make-instance 'ver :node "A")
-			    :to   (make-instance 'ver :node "B")))
+  (defparameter *v1-v2* (make-instance 'edge
+			    :from (make-instance 'node :node "A")
+			    :to   (make-instance 'node :node "B")))
 
   (insert-to *v1-v2* *g*)
-  (insert-to (make-instance 'ver :node "C") *g*)
-  (insert-to (make-instance 'ver :node "D") *g*)
+  (insert-to (make-instance 'node :node "C") *g*)
+  (insert-to (make-instance 'node :node "D") *g*)
   )
 
 ;;(defparameter *v1-v2* (make-instance 'rib :from (graph-find-vertex-by-name *g* "A") :to   (graph-find-vertex-by-name *g* "B")))
 
-(with-open-file (os "~/quicklisp/local-projects/mnas/mnas-graph/123.gv" :direction :output :if-exists :supersede)
+(with-open-file (os "~/123.gv" :direction :output :if-exists :supersede)
   (to-graphviz *g* os))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -23,9 +23,8 @@
 (defparameter data
   '(("a" "b") ("a" "c") ("a" "d") ("b" "c") ("c" "d")))
 
-(with-open-file (os "~/quicklisp/local-projects/mnas/mnas-graph/123.gv" :direction :output :if-exists :supersede)
-  (to-graphviz
-   (generate-graph
+(defparameter data-1
+     (generate-graph
     '((#1="Ванцовский" #2="Коротич")
       (#1#             #3="Петельчиц")
       (#3#             "Федоров")
@@ -39,6 +38,13 @@
       (#2#             "Матвеев")
       (#4#             "Иванов")
       (#2#             "Давлеткужин")
-      (#4#             "Гришина")))
-   os))
+      (#4#             "Гришина"))))
 
+(with-open-file (os "~/quicklisp/local-projects/mnas/mnas-graph/123.gv" :direction :output :if-exists :supersede)
+  (to-graphviz data-1 os))
+
+(view-graph data-1 "cool")
+
+(with-open-file (out (concatenate 'string fpath "/" fname ".gv")
+		       :direction :output :if-exists :supersede :external-format :UTF8)
+    (to-graphviz g out))
