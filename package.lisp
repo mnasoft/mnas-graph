aba74c7fb00f4c51aceda92c72416ec93d9bf68f
Nick Matvyeyev <mnasoft@gmail.com>
2020-04-07 22:07:02 +0300

    Вт 07 апр 2020 22:07:02 EEST

--- package.lisp
+++
@@ -1,9 +0,0 @@
-;;;; package.lisp
-
-
-(defpackage #:mnas-graph
-  (:use #:cl #:mnas-hash-table))
-
-;;;; (declaim (optimize (compilation-speed 0) (debug 3) (safety 0) (space 0) (speed 0)))
-
-(setf sb-impl::*default-external-format* :utf8)
