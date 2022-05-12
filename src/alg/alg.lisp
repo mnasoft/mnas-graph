060681f2904ab3338d910535b58426a7a8f6a750
Nick Matvyeyev <mnasoft@gmail.com>
2022-05-12 10:12:49 +0300

    12 май 2022 г.  7:12:49

--- src/alg/alg.lisp
+++ src/alg/alg.lisp
@@ -17,7 +17,9 @@
   графа @b(graph)."
    ))
 
-(defgeneric init-distance-graph (beg-node graph)
+(defgeneric init-distance-graph (beg-node graph
+                                 &key
+                                   (direction :forward) #+nil(direction :backward) #+nil(direction :both))
   (:documentation
    "@b(Описание:) обобщенная функция @b(init-distance-graph) возвращает
  граф @b(graph), модифицируя значения, связанные с вершинами данными
@@ -128,9 +130,25 @@
                  #'(lambda (node)
                      (mnas-hash-table:keys (outlet-edges node graph)))
                  l-nodes)))
-       (for-edge (edge beg end)
-         
-         )
+       (for-edge (edge beg-func end-func)  
+         (let ((end (funcall end-func edge)) 
+               (beg (funcall beg-func edge))) 
+           (let ((lst-end (gethash end (nodes graph)))
+                 (lst-beg (gethash beg (nodes graph))))
+             (cond
+               ((null (first lst-end))
+                (setf (gethash end (nodes graph))
+                      (list
+                       (+ (weight edge) (first lst-beg))
+                       beg))) 
+               ((and (numberp (first lst-end)) 
+                     (< (+ (weight edge) (first lst-beg))
+                        (first lst-end))) 
+                (setf (gethash end (nodes graph))
+                      (list
+                       (+ (weight edge) (first lst-beg)) 
+                       beg)))) 
+             end)))
        (for-edges (edges)
          "@b(Описание:) функция @b(for-edges) возвращает список вершин,
  расположенных в головах ребер @b(edges). В качестве побочного эффекта
@@ -142,24 +160,7 @@
  длиной."
          (mapcar
           #'(lambda (edge)
-              (let ((end (head edge))
-                    (beg (tail edge)))
-                (let ((lst-end (gethash end (nodes graph)))
-                      (lst-beg (gethash beg (nodes graph))))
-                  (cond
-                    ((null (first lst-end))
-                     (setf (gethash end (nodes graph))
-                           (list
-                            (+ (weight edge) (first lst-beg))
-                            beg)))
-                    ((and (numberp (first lst-end))
-                          (< (+ (weight edge) (first lst-beg))
-                             (first lst-end)))
-                     (setf (gethash end (nodes graph))
-                           (list
-                            (+ (weight edge) (first lst-beg))
-                            beg))))
-                  end)))
+              (for-edge edge #'tail #'head))
           edges)))
     (when (not (nth-value 1 (gethash beg-node (nodes graph))))
       (error "The node=~S is not into ~%graph=~S " beg-node graph))
@@ -180,59 +181,60 @@
 (defmethod init-distance-graph-inlet ((beg-node <node>) (graph <graph>))
 ;;; Проверка на принадлежность начальной @b(beg-node) и конечной
 ;;; @b(end-node) вершины графу @b(graph)
-  (labels
-      ((init ()
-         "@b(Описание:) функция @b(init) инициализирует значения для каждой из
-  вершин графа списком (w n), где w=nil - сумма весов предыдущих
-  вершин; n=nil -предыдущая вершина). Для начальной вершины
-  @(beg-node) w=0; n=nil.
- "
-         (let ((ht-nodes (nodes graph))
-               (nodes (mnas-hash-table:keys (nodes graph))))
-           (mapcar
-            #'(lambda (node)
-                (setf (gethash node ht-nodes) '(nil nil)))
-            nodes)
-           (setf (gethash beg-node ht-nodes)  '(0 nil))))
-       (for-nodes (l-nodes)
-         "@b(Описание:) функция @b(for-nodes) возвращает список исходящих ребер
- для вершин графа @b(graph), заданных списком @b(l-nodes)."
-         (apply #'append
-                (mapcar
-                 #'(lambda (node)
-                     (mnas-hash-table:keys (inlet-edges node graph))) ;; outlet-edges -> inlet-edges
-                 l-nodes)))
-       (for-edges (edges)
-         "@b(Описание:) функция @b(for-edges) возвращает список вершин,
- расположенных в головах ребер @b(edges). В качестве побочного эффекта
- модифицирует значения хеш-таблицы вершин так, что для значение для
- определенной вершины является списком двух элементов. Первый элемент
- этого списка является числом, представляющим минимальную длину пути,
- найденную к этой вершине от начальной вершины. Второй элемент
- является предыдущей вершиной, для которой найден путь с минимальной
- длиной.
+  (labels (
+           (init ()
+             "@b(Описание:) функция @b(init) инициализирует значения для каждой из
+              вершин графа списком (w n), где w=nil - сумма весов
+              предыдущих вершин; n=nil -предыдущая вершина). Для
+              начальной вершины @(beg-node) w=0; n=nil."
+             (let ((ht-nodes (nodes graph))
+                   (nodes (mnas-hash-table:keys (nodes graph))))
+               (mapcar
+                #'(lambda (node)
+                    (setf (gethash node ht-nodes) '(nil nil)))
+                nodes)
+               (setf (gethash beg-node ht-nodes)  '(0 nil))))
+           (for-nodes (l-nodes)
+             "@b(Описание:) функция @b(for-nodes) возвращает список исходящих ребер
+              для вершин графа @b(graph), заданных списком
+              @b(l-nodes)."
+             (apply #'append
+                    (mapcar
+                     #'(lambda (node)
+                         (mnas-hash-table:keys (inlet-edges node graph))) ;; outlet-edges -> inlet-edges
+                     l-nodes)))
+           (for-edge (edge beg-func end-func)  
+             (let ((end (funcall end-func edge))  ;; (tail edge) -> end-func
+                   (beg (funcall beg-func edge))) ;; (head edge) -> beg-func
+               (let ((lst-end (gethash end (nodes graph)))
+                     (lst-beg (gethash beg (nodes graph))))
+                 (cond
+                   ((null (first lst-end))
+                    (setf (gethash end (nodes graph))
+                          (list
+                           (+ (weight edge) (first lst-beg))
+                           beg))) 
+                   ((and (numberp (first lst-end)) 
+                         (< (+ (weight edge) (first lst-beg))
+                            (first lst-end))) 
+                    (setf (gethash end (nodes graph))
+                          (list
+                           (+ (weight edge) (first lst-beg)) 
+                           beg)))) 
+                 end)))
+           (for-edges (edges)
+             "@b(Описание:) функция @b(for-edges) возвращает список вершин,
+расположенных в головах ребер @b(edges). В качестве побочного эффекта
+модифицирует значения хеш-таблицы вершин так, что для значение для
+определенной вершины является списком двух элементов. Первый элемент
+этого списка является числом, представляющим минимальную длину пути,
+найденную к этой вершине от начальной вершины. Второй элемент является
+предыдущей вершиной, для которой найден путь с минимальной длиной.
 "
-         (mapcar
-          #'(lambda (edge)
-              (let ((end (tail edge))
-                    (beg (head edge)))
-                (let ((lst-end (gethash end (nodes graph)))
-                      (lst-beg (gethash beg (nodes graph))))
-                  (cond
-                    ((null (first lst-end))
-                     (setf (gethash end (nodes graph))
-                           (list
-                            (+ (weight edge) (first lst-beg))
-                            beg))) 
-                    ((and (numberp (first lst-end)) 
-                          (< (+ (weight edge) (first lst-beg))
-                             (first lst-end))) 
-                     (setf (gethash end (nodes graph))
-                           (list
-                            (+ (weight edge) (first lst-beg)) 
-                            beg)))) 
-                  end)))
-          edges)))
+             (mapcar
+              #'(lambda (edge)
+                  (for-edge edge #'head #'tail))
+              edges)))
     (when (not (nth-value 1 (gethash beg-node (nodes graph))))
       (error "The node=~S is not into ~%graph=~S " beg-node graph))
     (let ((ht-e (make-hash-table))    ; хеш-таблица, встреченных ребер
@@ -242,14 +244,16 @@
       (do* ((n-s (list beg-node) (for-edges e-s))
             (e-s (for-nodes n-s) (for-nodes n-s)))
            ((= c-e (hash-table-count ht-e)) (view graph))
-        (setf c-e (hash-table-count ht-e));; предыдущее количество обработанных ребер
+        (setf c-e (hash-table-count ht-e)) ;; предыдущее количество обработанных ребер
         (map nil ;; добавление обработанных ребер в хеш-таблицу
              #'(lambda (edge)
                  (setf (gethash edge ht-e) nil))
              e-s))
       graph)))
 
-(defmethod path ((beg-node <node>) (end-node <node>) (graph <graph>))
+(defmethod path ((beg-node <node>) (end-node <node>) (graph <graph>)
+                 &key (direction :forward) #+nil(direction :backward) #+nil(direction :both)
+                   )
 ;;; Проверка на принадлежность начальной @b(beg-node) и конечной
 ;;; @b(end-node) вершины графу @b(graph)
   (when (or (not (nth-value 1 (gethash beg-node (nodes graph))))
@@ -279,11 +283,8 @@
   (defparameter *g* (copy mnas-graph/tests::*g*))
   (view *g*)
   (path (find-node "a" *g*) (find-node "f" *g*) *g*)
-  (path (find-node "g" *g*) (find-node "a" *g*) *g*))
+  (path (find-node "g" *g*) (find-node "a" *g*) *g*)
 
-(first (gethash (find-node "f" *g*) (nodes *g*)))
-
-(init-distance-graph (find-node "a" *g*) *g*)
-(view *g*)
-
-(mnas-graph/view:view-graph *g*)
+  (init-distance-graph (find-node "a" *g*) *g*)
+  (mnas-graph/view:view-graph *g*)
+  (view *g*))
